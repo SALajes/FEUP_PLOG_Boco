7053c38e3a56eeb398b9ceb0cc68dc29d982b02c
Carlos Nova Duarte <cnova1998@gmail.com>
2019-10-12 20:54:39 +0100

    Board representation is now identical to Boco game

--- board.pl
+++ board.pl
@@ -1,28 +1,77 @@
-printVertLine(0) :-
-    write(│),
+even(X) :-
+    0 is X mod 2.
+odd(X) :-
+    1 is X mod 2.
+
+printVertLine(_, 0) :-
+    write('|'),
     nl.
 
-printVertLine(Rows) :-
-    write('│     '),
-    Rows2 is Rows-1,
-    printVertLine(Rows2).
-
-printHoriLine(0) :-
-    write(─────),
-    write(─────),
+printVertLine(Row, _Col) :-
+    Row == 10,
+    write('|   |               |                   |'),
     nl.
 
-printHoriLine(Rows) :-
-    write(─────),
-    Rows2 is Rows-1,
-    printHoriLine(Rows2).
+printVertLine(Row, _Col) :-
+    Row == 1,
+    write('|               |                   |   |'),
+    nl.
 
-printEmptyBoard(0, Cols) :-
-    printHoriLine(Cols).
+printVertLine(Row, Col) :-
+    Col\=1,
+    Col\=10,
+    Row\=1,
+    Row\=10,
+    even(Row),
+    odd(Col),
+    write('| \\ '),
+    Col2 is Col-1,
+    printVertLine(Row, Col2).
+
+printVertLine(Row, Col) :-
+    Col\=1,
+    Col\=10,
+    Row\=1,
+    Row\=10,
+    odd(Row),
+    even(Col),
+    write('| / '),
+    Col2 is Col-1,
+    printVertLine(Row, Col2).
+
+printVertLine(Row, Col) :-
+    write('|   '),
+    Col2 is Col-1,
+    printVertLine(Row, Col2).
+
+
+printVertLine(Row, Col) :-
+    write('|   '),
+    Col2 is Col-1,
+    printVertLine(Row, Col2).
+
+printHoriLine(_, 0) :-
+    write(-),
+    nl.
+
+printHoriLine(Row, Col) :-
+    (((Col == 1, (Row == 1; Row == 2; Row == 3; Row == 4;
+Row == 6; Row == 7; Row == 8)));
+    (Col == 10, (Row == 2; Row == 3; Row == 4; Row == 5; Row == 7; Row == 8; Row == 9))),
+    write('    '),
+    Col2 is Col - 1,
+    printHoriLine(Row, Col2).
+
+printHoriLine(Row, Col) :-
+    write(----),
+    Col2 is Col-1,
+    printHoriLine(Row, Col2).
+
+printEmptyBoard(0, _) :-
+    printHoriLine(10, 10).
 
 printEmptyBoard(Rows, Cols) :-
+    printHoriLine(Rows, Cols),
+    printVertLine(Rows, Cols),
     Aux is Rows-1,
-    printHoriLine(Cols),
-    printVertLine(Cols),
-    printVertLine(Cols),
     printEmptyBoard(Aux, Cols).
