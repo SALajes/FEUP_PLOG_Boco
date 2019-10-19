d8cffe61f802d30c7349ac33ed5719752a6f8db7
Carlos Nova Duarte <cnova1998@gmail.com>
2019-10-20 00:21:35 +0100

    Added some plays to updateBoard predicate

--- game.pl
+++ game.pl
@@ -1,10 +1,13 @@
 :- include('board.pl').
 
 updateBoard(Board, Player) :-
-    print_board(Board, 1), 
     % makeMove(Player, 4, 8, Board, NewBoard),
-    makeMove(Player, 9, 2, right, Board, NewBoard),
-    print_board(NewBoard, 1).
+    makeMove(Player, 2, 3, right, Board, NewBoard),
+    makeMove(p2, 2, 4, NewBoard, NewBoard1),
+    makeMove(Player, 3, 4, left, NewBoard1, NewBoard2),
+    makeMove(p2, 3, 4, right, NewBoard2, NewBoard3),
+    makeMove(Player, 3, 5, NewBoard3, NewBoard4),
+    print_board(NewBoard4, 1).
 
 % Starts the game, printing an empty board
 % Need to provide the size of the map (rows and columns)
