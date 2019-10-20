8a1e74ea554c98b225d92f4f9ffbf1333d94e765
cadu98 <cnova1998@gmail.com>
2019-10-21 00:00:49 +0100

    Updated display_game predicate

--- game.pl
+++ game.pl
@@ -1,16 +1,16 @@
 :- include('board.pl').
 
-display_game(Board, Player) :-
-    makeMove(Player, 4, 8, Board, NewBoard),
-    makeMove(Player, 2, 3, right, NewBoard, NewBoard2),
-    makeMove(p2, 2, 4, NewBoard2, NewBoard3),
-    makeMove(Player, 3, 4, left, NewBoard3, NewBoard4),
-    makeMove(p2, 3, 4, right, NewBoard4, NewBoard5),
-    makeMove(Player, 3, 5, NewBoard5, NewBoard6),
-    print_board(NewBoard6, 1).
+display_game(Board, _Player) :-
+    % makeMove(Player, 4, 8, Board, NewBoard),
+    % makeMove(Player, 2, 3, right, NewBoard, NewBoard2),
+    % makeMove(p2, 2, 4, NewBoard2, NewBoard3),
+    % makeMove(Player, 3, 4, left, NewBoard3, NewBoard4),
+    % makeMove(p2, 3, 4, right, NewBoard4, NewBoard5),
+    % makeMove(Player, 3, 5, NewBoard5, NewBoard6),
+    print_board(Board, 1).
 
 % Starts the game, printing an empty board
 % Need to provide the size of the map (rows and columns)
 play :-
-    bocoStructure(Board),
+    initialBoard(Board),
     display_game(Board, p1).
