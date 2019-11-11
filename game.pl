a0a58357a12091cfa5cb5e243b673c58fd70db91
cadu98 <cnova1998@gmail.com>
2019-11-11 15:04:29 +0000

    Now it's possible to check valid cells

--- game.pl
+++ game.pl
@@ -66,6 +66,204 @@ execute_play(Board, Player) :-
     ite(Player==p1, NewPlayer=p2, NewPlayer=p1),
     execute_play(NewBoard, NewPlayer).
 
+cell_empty(Board, Row, Column) :-
+    valid_coordinate(Row),
+    valid_coordinate(Column),
+    get_cell(Board, Row, Column, Cell),
+    nth1(1, Cell, 'R'),
+    nth1(3, Cell, nill).
+
+cell_empty(Board, Row, Column) :-
+    valid_coordinate(Row),
+    valid_coordinate(Column),
+    get_cell(Board, Row, Column, Cell),
+    nth1(1, Cell, 'Q'),
+    nth1(2, Cell, nill).
+
+cell_empty(Board, Row, Column, Side) :-
+    valid_coordinate(Row),
+    valid_coordinate(Column),
+    get_cell(Board, Row, Column, Cell),
+    nth1(1, Cell, 'T'),
+    (   Side==left,
+        nth1(3, Cell, nill)
+    ;   Side==right,
+        nth1(4, Cell, nill)
+    ).
+    
+%When a triangle is of type 'up', checks if the cell above Cell is
+%valid to be selected.
+check_above(Board, Row, Column, Side) :-
+    NewRow is Row-1,
+    valid_coordinate(NewRow),
+    get_cell(Board, Row, Column, Cell),
+    nth1(2, Cell, up),
+    ite(Side==right,
+        \+cell_empty(Board, Row, Column, left),
+        it(Side==left, \+cell_empty(Board, NewRow, Column))).
+
+check_above(Board, Row, Column, Side) :-
+    NewRow is Row-1,
+    valid_coordinate(NewRow),
+    get_cell(Board, Row, Column, Cell),
+    nth1(2, Cell, dw),
+    ite(Side==right,
+        \+cell_empty(Board, NewRow, Column),
+        it(Side==left, \+cell_empty(Board, Row, Column, right))).
+
+check_above(Board, Row, Column) :-
+    NewRow is Row-1,
+    valid_coordinate(NewRow),
+    get_cell(Board, Row, Column, Cell),
+    nth1(1, Cell, 'Q'),
+    get_cell(Board, NewRow, Column, AboveCell),
+    ite(nth1(1, AboveCell, 'R'),
+        \+cell_empty(Board, NewRow, Column),
+        ite(nth1(2, AboveCell, up),
+            \+cell_empty(Board, NewRow, Column, right),
+            \+cell_empty(Board, NewRow, Column, left))).
+
+check_above(Board, Row, Column) :-
+    NewRow is Row-1,
+    valid_coordinate(NewRow),
+    get_cell(Board, Row, Column, Cell),
+    nth1(1, Cell, 'R'),
+    get_cell(Board, NewRow, Column, AboveCell),
+    nth1(1, AboveCell, 'R'),
+    \+cell_empty(Board, NewRow, Column).
+
+check_above(Board, Row, Column) :-
+    NewRow is Row-1,
+    valid_coordinate(NewRow),
+    get_cell(Board, Row, Column, Cell),
+    nth1(1, Cell, 'R'),
+    get_cell(Board, NewRow, Column, AboveCell),
+    ite(nth1(1, AboveCell, 'Q'),
+        \+cell_empty(Board, NewRow, Column),
+        \+cell_empty(Board, NewRow, Column, right)).
+
+check_below(Board, Row, Column, Side) :-
+    NewRow is Row+1,
+    valid_coordinate(NewRow),
+    get_cell(Board, Row, Column, Cell),
+    nth1(2, Cell, up),
+    ite(Side==right,
+        \+cell_empty(Board, NewRow, Column),
+        it(Side==left, \+cell_empty(Board, Row, Column, right))).
+
+check_below(Board, Row, Column, Side) :-
+    NewRow is Row+1,
+    valid_coordinate(NewRow),
+    get_cell(Board, Row, Column, Cell),
+    nth1(2, Cell, dw),
+    ite(Side==right,
+        \+cell_empty(Board, NewRow, Column, left),
+        it(Side==left, \+cell_empty(Board, Row, Column))).
+
+check_below(Board, Row, Column) :-
+    NewRow is Row+1,
+    valid_coordinate(NewRow),
+    get_cell(Board, Row, Column, Cell),
+    nth1(1, Cell, 'Q'),
+    get_cell(Board, NewRow, Column, BelowCell),
+    ite(nth1(1, BelowCell, 'R'),
+        \+cell_empty(Board, NewRow, Column),
+        ite(nth1(2, BelowCell, up),
+            \+cell_empty(Board, NewRow, Column, left),
+            \+cell_empty(Board, NewRow, Column, right))).
+
+check_below(Board, Row, Column) :-
+    NewRow is Row+1,
+    valid_coordinate(NewRow),
+    get_cell(Board, Row, Column, Cell),
+    nth1(1, Cell, 'R'),
+    get_cell(Board, NewRow, Column, BelowCell),
+    ite(nth1(1, BelowCell, 'Q'),
+        \+cell_empty(Board, NewRow, Column),
+        \+cell_empty(Board, NewRow, Column, right)).
+
+check_below(Board, Row, Column) :-
+    NewRow is Row+1,
+    valid_coordinate(NewRow),
+    get_cell(Board, Row, Column, Cell),
+    nth1(1, Cell, 'R'),
+    get_cell(Board, NewRow, Column, BelowCell),
+    nth1(1, BelowCell, 'R'),
+    \+cell_empty(Board, NewRow, Column).
+
+check_left(Board, Row, Column, Side) :-
+    NewColumn is Column-1,
+    valid_coordinate(NewColumn),
+    ite(Side==left,
+        \+cell_empty(Board, Row, NewColumn),
+        \+cell_empty(Board, Row, Column, left)).
+
+check_left(Board, Row, Column) :-
+    NewColumn is Column-1,
+    valid_coordinate(NewColumn),
+    get_cell(Board, Row, Column, Cell),
+    nth1(1, Cell, 'Q'),
+    get_cell(Board, Row, NewColumn, LeftCell),
+    ite(nth1(1, LeftCell, 'R'),
+        \+cell_empty(Board, Row, NewColumn),
+        \+cell_empty(Board, Row, NewColumn, right)).
+
+
+check_left(Board, Row, Column) :-
+    NewColumn is Column-1,
+    valid_coordinate(NewColumn),
+    get_cell(Board, Row, Column, Cell),
+    nth1(1, Cell, 'R'),
+    get_cell(Board, Row, NewColumn, LeftCell),
+    ite((nth1(1, LeftCell, 'R');nth1(1, LeftCell, 'Q')),
+        \+cell_empty(Board, Row, NewColumn),
+        \+cell_empty(Board, Row, NewColumn, right)).
+
+check_right(Board, Row, Column, Side) :-
+    NewColumn is Column+1,
+    valid_coordinate(NewColumn),
+    ite(Side==right,
+        \+cell_empty(Board, Row, NewColumn),
+        \+cell_empty(Board, Row, Column, right)).
+
+check_right(Board, Row, Column) :-
+    NewColumn is Column+1,
+    valid_coordinate(NewColumn),
+    get_cell(Board, Row, Column, Cell),
+    nth1(1, Cell, 'Q'),
+    get_cell(Board, Row, NewColumn, RightCell),
+    ite(nth1(1, RightCell, 'R'),
+        \+cell_empty(Board, Row, NewColumn),
+        \+cell_empty(Board, Row, NewColumn, left)).
+
+check_right(Board, Row, Column) :-
+    NewColumn is Column+1,
+    valid_coordinate(NewColumn),
+    get_cell(Board, Row, Column, Cell),
+    nth1(1, Cell, 'R'),
+    get_cell(Board, Row, NewColumn, RightCell),
+    ite((nth1(1, RightCell, 'R');nth1(1, RightCell, 'Q')),
+        \+cell_empty(Board, Row, NewColumn),
+        \+cell_empty(Board, Row, NewColumn, left)).
+
+valid_cell(Board, Row, Column, Side) :-
+    get_cell(Board, Row, Column, Cell),
+    nth1(1, Cell, 'T'),
+    cell_empty(Board, Row, Column, Side),
+    (   check_above(Board, Row, Column, Side)
+    ;   check_below(Board, Row, Column, Side)
+    ;   check_left(Board, Row, Column, Side)
+    ;   check_right(Board, Row, Column, Side)
+    ).
+
+valid_cell(Board, Row, Column) :-
+    cell_empty(Board, Row, Column),
+    (   check_above(Board, Row, Column)
+    ;   check_below(Board, Row, Column)
+    ;   check_left(Board, Row, Column)
+    ;   check_right(Board, Row, Column)
+    ).
+
 check_single(Cell, Id, Player, List, Board, NewBoard, Row, Column) :-
     ite(is_rectangle(Cell, Id),
         update_board_multiple(Player, List, Board, NewBoard),
--- internalstructure.pl
+++ internalstructure.pl
@@ -30,14 +30,14 @@ initialBoard([
 
 middleBoard([
     [['R',8,nill], ['R',1,nill],['R',1,nill],['R',1,nill],['R',1,nill], ['R',2,nill],['R',2,nill],['R',2,nill],['R',2,nill],['R',2,nill]],
-    [['R',8,nill], ['Q',1,nill], ['T',1,dw,nill,nill], ['Q',2,nill], ['T',2,dw,nill,nill], ['Q',3,nill], ['T',3,dw,nill,nill], ['Q',4,nill], ['T',4,dw,nill,nill], ['R',3,nill]],
-    [['R',8,nill], ['T',5,up,nill,nill], ['Q',5,nill], ['T',6,up,nill,nill], ['Q',6,nill], ['T',7,up,nill,nill], ['Q',7,nill], ['T',8,up,nill,nill], ['Q',8,nill], ['R',3,nill]],
-    [['R',8,nill], ['Q',9,nill], ['T',9,dw,nill,nill], ['Q',10,p1], ['T',10,dw,p2,p1], ['Q',11,nill], ['T',11,dw,nill,nill], ['Q',12,nill], ['T',12,dw,nill,nill], ['R',3,nill]],
-    [['R',8,nill], ['T',13,up,nill,nill], ['Q',13,nill], ['T',14,up,p2,p1], ['Q',14,nill], ['T',15,up,nill,nill], ['Q',15,nill], ['T',16,up,nill,nill], ['Q',16,nill], ['R',3,nill]],
-    [['R',7,nill], ['Q',17,nill], ['T',17,dw,nill,nill], ['Q',18,p2], ['T',18,dw,p1,p2], ['Q',19,nill], ['T',19,dw,nill,nill], ['Q',20,nill], ['T',20,dw,nill,nill], ['R',4,nill]],
-    [['R',7,nill], ['T',21,up,nill,nill], ['Q',21,nill], ['T',22,up,nill,nill], ['Q',22,p1], ['T',23,up,nill,nill], ['Q',23,nill], ['T',24,up,nill,nill], ['Q',24,nill], ['R',4,nill]],
-    [['R',7,nill], ['Q',25,nill], ['T',25,dw,nill,nill], ['Q',26,nill], ['T',26,dw,nill,nill], ['Q',27,nill], ['T',27,dw,nill,nill], ['Q',28,nill], ['T',28,dw,nill,nill], ['R',4,nill]],
-    [['R',7,nill], ['T',29,up,nill,nill], ['Q',29,nill], ['T',30,up,nill,nill], ['Q',30,nill], ['T',31,up,nill,nill], ['Q',31,nill], ['T',32,up,nill,nill], ['Q',32,nill], ['R',4,nill]],
+    [['R',8,nill], ['Q',nill], ['T',dw,nill,nill], ['Q',nill], ['T',dw,nill,nill], ['Q',nill], ['T',dw,nill,nill], ['Q',nill], ['T',dw,nill,nill], ['R',3,nill]],
+    [['R',8,nill], ['T',up,nill,nill], ['Q',nill], ['T',up,nill,nill], ['Q',nill], ['T',up,nill,nill], ['Q',nill], ['T',up,nill,nill], ['Q',nill], ['R',3,nill]],
+    [['R',8,nill], ['Q',nill], ['T',dw,nill,nill], ['Q',p1], ['T',dw,p2,p1], ['Q',nill], ['T',dw,nill,nill], ['Q',nill], ['T',dw,nill,nill], ['R',3,nill]],
+    [['R',8,nill], ['T',up,nill,nill], ['Q',nill], ['T',up,p2,p1], ['Q',nill], ['T',up,nill,nill], ['Q',nill], ['T',up,nill,nill], ['Q',nill], ['R',3,nill]],
+    [['R',7,nill], ['Q',nill], ['T',dw,nill,nill], ['Q',p2], ['T',dw,p1,p2], ['Q',nill], ['T',dw,nill,nill], ['Q',nill], ['T',dw,nill,nill], ['R',4,nill]],
+    [['R',7,nill], ['T',up,nill,nill], ['Q',nill], ['T',up,nill,nill], ['Q',p1], ['T',up,nill,nill], ['Q',nill], ['T',up,nill,nill], ['Q',nill], ['R',4,nill]],
+    [['R',7,nill], ['Q',nill], ['T',dw,nill,nill], ['Q',nill], ['T',dw,nill,nill], ['Q',nill], ['T',dw,nill,nill], ['Q',nill], ['T',dw,nill,nill], ['R',4,nill]],
+    [['R',7,nill], ['T',up,nill,nill], ['Q',nill], ['T',up,nill,nill], ['Q',nill], ['T',up,nill,nill], ['Q',nill], ['T',up,nill,nill], ['Q',nill], ['R',4,nill]],
     [['R',6,nill],['R',6,nill],['R',6,nill],['R',6,nill],['R',6,nill], ['R',5,nill],['R',5,nill],['R',5,nill],['R',5,nill], ['R',4,nill]]
 ]).
 
